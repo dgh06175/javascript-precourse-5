376444c993d1bdf21e0fd6f18ca3e27df012aaab
dgh06175 <dgh06175@gmail.com>
2022-11-01 22:43:32 +0900

    hw1

--- src/problem4.js
+++ src/problem4.js
@@ -1,5 +1,23 @@
 function problem4(word) {
-  var answer;
+  var answer = "";
+  var low = "abcdefghijklmnopqrstuvwxyz";
+  var upp = "ABCDEFGHIJKLMNOPQRSTUVWSYZ";
+  for (var i=0;i<word.length;i++)
+  {
+    if (word[i] == ' '){
+      answer = answer + ' ';
+    }
+    else if (word[i].toLowerCase() == word[i]) // 소문자
+    {
+        a = word[i].charCodeAt(0) - 'a'.charCodeAt(0);
+        answer = answer + low[25 - a];
+    }
+    else if (word[i].toUpperCase() == word[i]) // 대문자
+    {
+        a = word[i].charCodeAt(0) - 'A'.charCodeAt(0);
+        answer = answer + upp[25 - a];
+    }
+  }
   return answer;
 }
 
--- src/problem5.js
+++ src/problem5.js
@@ -1,5 +1,12 @@
 function problem5(money) {
-  var answer;
+  var answer = [0,0,0,0,0,0,0,0,0];
+  var bills = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+  for (var i = 0; i < 9; i++){
+    if (money >= bills[i]){
+      answer[i] += parseInt(money / bills[i]);
+      money -= parseInt(money / bills[i]) * bills[i];
+    }
+  }
   return answer;
 }
 
