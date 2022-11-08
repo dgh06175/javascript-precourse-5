36d7a8e9a0a3874129fc8c2e010262d1747720f4
dgh06175 <dgh06175@gmail.com>
2022-11-08 14:15:04 +0900

    feat: playGame Add 게임을 진행하는 메소드인 playGame 메소드 추가

--- src/App.js
+++ src/App.js
@@ -28,6 +28,29 @@ class App {
     return randomArr;
     // randomArr = [ 3, 9, 7 ]
   }
+
+  playGame(computerNumber) {
+    const playerNumber = playerInputForGame();
+    const ballStrike = countBallStrike(computerNumber, playerNumber);
+    const ball = ballStrike[0];
+    const strike = ballStrike[1];
+    let ballStrikeText = "";
+    if (ball === 0 && strike === 0) {
+      ballStrikeText += "낫싱";
+    }
+    if (ball > 0) {
+      ballStrikeText += `${ball}볼`;
+    }
+    if (strike > 0) {
+      ballStrikeText += ` ${strike}스트라이크`;
+    }
+    MissionUtils.Console.print(ballStrikeText);
+    if (strike === 3) {
+      MissionUtils.Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+      return "finish";
+    }
+    return "continue";
+  }
 }
 
 const a = new App();
