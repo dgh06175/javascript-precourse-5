e0796c3e81f14d74773ac41d7307851fcec09d20
dgh06175 <dgh06175@gmail.com>
2022-11-08 18:44:40 +0900

    feat: Add continueGame 메소드 추가. 테스트 통과

--- src/App.js
+++ src/App.js
@@ -83,6 +83,22 @@ class App {
     } // 반복
   }
 
+  continueGame() {
+    Console.readLine(
+      "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n",
+      (answer) => {
+        if (answer === "1") {
+          this.comNumberInit();
+          this.playerInput(); // 재귀
+        }
+        if (answer === "2") {
+          MissionUtils.Console.print("게임 종료");
+        }
+        return;
+      }
+    );
+  }
+
   play() {
     MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
     this.comNumberInit();
