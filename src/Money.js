f3d2991b7e030e2d2acea1ea3055158b4d6066ca
내 이름 <  >
2022-11-10 23:48:36 +0900

    feat: add Money.js

--- src/Money.js
+++ src/Money.js
@@ -1,1 +1,31 @@
-class Money(){}
+/* eslint-disable quotes */
+const MissionUtils = require("@woowacourse/mission-utils");
+
+const { Console } = MissionUtils;
+
+class Money {
+  #money;
+
+  constructor(money) {
+    this.#money = money;
+    this.check = {
+      isThousand: (answer) => answer % 1000 === 0,
+      isInt: (answer) => Number.isInteger(+answer),
+    };
+  }
+
+  inputMoney() {
+    Console.readLine("구입금액을 입력해 주세요.", (answer) => {
+      if (!this.validateForMoney(answer)) {
+        throw new Error("잘못된 입력입니다..");
+      }
+    });
+  }
+
+  validateForMoney(money) {
+    const { isThousand, isInt } = this.check;
+    if (!isThousand(money) || !isInt(money)) {
+      return false;
+    }
+  }
+}
