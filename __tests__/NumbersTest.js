b29f4369e37eb3fe561577c386dcb6cefe65a8e1
내 이름 <  >
2022-11-15 22:40:08 +0900

    feat: test code add

--- __tests__/NumbersTest.js
+++ __tests__/NumbersTest.js
@@ -2,7 +2,7 @@ const Numbers = require('../src/Numbers');
 
 const LOTTO_PRICE = 1000;
 
-describe('머니 클래스 테스트', () => {
+describe('Numbers 클래스 테스트', () => {
   test('1000의 배수 테스트', () => {
     const number = new Numbers();
     const result = number.validateForMoney('21010');
@@ -15,7 +15,7 @@ describe('머니 클래스 테스트', () => {
     expect(result).toEqual(false);
   });
 
-  test('문자열 입력 테스트', () => {
+  test('정상 입력 테스트', () => {
     const number = new Numbers();
     const result = number.validateForMoney('21000');
     expect(result).toEqual(true);
--- __tests__/ResultTest.js
+++ __tests__/ResultTest.js
@@ -1,7 +1,7 @@
 const Result = require('../src/Result');
 
 describe('result 클래스 테스트', () => {
-  test('1000의 배수 테스트', () => {
+  test('수익률 테스트', () => {
     const r = new Result();
     const winCount = {
       sameNumberis3: 1,
