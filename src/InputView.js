1e294099f2b54ef152803d7cdf33ecb46fc537e5
내 이름 <  >
2022-11-22 00:44:19 +0900

    feat: InputView bind 사용한 콜백으로 작성

--- src/InputView.js
+++ src/InputView.js
@@ -1,3 +1,6 @@
+const MissionUtils = require('@woowacourse/mission-utils');
+
+const { Console } = MissionUtils;
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  * 제공된 InputView 객체를 활용해 구현해야 한다.
@@ -9,17 +12,36 @@ const InputView = {
   /**
    * 다리의 길이를 입력받는다.
    */
-  readBridgeSize() {},
+  readBridgeSize(callback) {
+    Console.readLine('\n다리의 길이를 입력해주세요.\n', (answer) => {
+      callback.bind(this)(answer);
+    });
+  },
 
   /**
    * 사용자가 이동할 칸을 입력받는다.
    */
-  readMoving() {},
+  readMoving(callback) {
+    Console.readLine(
+      '\n이동할 칸을 선택해주세요. (위: U, 아래: D)\n',
+      (answer) => {
+        callback.bind(this)(answer);
+      }
+    );
+  },
 
   /**
    * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
    */
-  readGameCommand() {},
+
+  readGameCommand(callback) {
+    Console.readLine(
+      '\n게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)\n',
+      (answer) => {
+        callback.bind(this)(answer);
+      }
+    );
+  },
 };
 
 module.exports = InputView;
