bc5ccb37c24f64f8af122d8d7be55c3cfa53a1e0
내 이름 <  >
2022-11-22 01:29:49 +0900

    feat: getBridge 메서드 추가

--- src/Game.js
+++ src/Game.js
@@ -1,11 +1,22 @@
 const InputView = require('./InputView');
 const OutputView = require('./OutputView');
 const BridgeMaker = require('./BridgeMaker');
+const BridgeRandomNumberGenerator = require('./BridgeRandomNumberGenerator');
 
 class Game {
+  #bridge;
+
   initGame() {
     OutputView.startOutput();
-    InputView.readBridgeSize.bind(this)(this.printInput);
+    InputView.readBridgeSize.bind(this)(this.getBridge);
+  }
+
+  getBridge(input) {
+    this.#bridge = BridgeMaker.makeBridge(
+      Number(input),
+      BridgeRandomNumberGenerator.generate
+    );
+    this.moveOneStep(0);
   }
 }
 
