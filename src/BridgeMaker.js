5e4580fd78170964bfd556fbd300b48be66cf6c5
내 이름 <  >
2022-11-22 01:29:29 +0900

    feat: makeBridge 메서드 추가

--- src/BridgeMaker.js
+++ src/BridgeMaker.js
@@ -11,7 +11,18 @@ const BridgeMaker = {
    * @param {function(): number} generateRandomNumber 무작위 값을 생성해주는 함수
    * @return {string[]} 입력받은 길이에 해당하는 다리 모양. 위 칸이면 U, 아래 칸이면 D로 표현해야 한다.
    */
-  makeBridge(size, generateRandomNumber) {},
+  makeBridge(size, generateRandomNumber) {
+    let bridge = '';
+    for (let i = 0; i < size; i += 1) {
+      const randomNumber = generateRandomNumber();
+      if (randomNumber === 0) {
+        bridge += 'D';
+      } else {
+        bridge += 'U';
+      }
+    }
+    return bridge;
+  },
 };
 
 module.exports = BridgeMaker;
