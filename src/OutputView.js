026ab9d34f9ef8b664fb0a0b63e77ed0f9af6274
내 이름 <  >
2022-11-22 00:43:17 +0900

    feat: OutputView - startOutput 추가

--- src/OutputView.js
+++ src/OutputView.js
@@ -1,3 +1,5 @@
+const MissionUtils = require('@woowacourse/mission-utils');
+const { Console } = MissionUtils;
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  * 제공된 OutputView 객체를 활용해 구현해야 한다.
@@ -7,19 +9,22 @@
 
  */
 const OutputView = {
+  startOutput() {
+    Console.print('다리 건너기 게임을 시작합니다.');
+  },
   /**
    * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
    * <p>
    * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
-  printMap() {},
+  //printMap() {},
 
   /**
    * 게임의 최종 결과를 정해진 형식에 맞춰 출력한다.
    * <p>
    * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
-  printResult() {},
+  //printResult() {},
 };
 
 module.exports = OutputView;
