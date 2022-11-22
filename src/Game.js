f209bebd29dc040e9392f517beea36f7b9725918
내 이름 <  >
2022-11-22 15:29:26 +0900

    chore: 변수명 변경

--- src/Game.js
+++ src/Game.js
@@ -7,7 +7,7 @@ const BridgeGame = require('./BridgeGame');
 class Game {
   #bridge;
 
-  #size;
+  #bridgeLength;
 
   #location;
 
@@ -17,11 +17,12 @@ class Game {
   }
 
   getBridge(input) {
-    this.#size = Number(input);
+    this.#bridgeLength = Number(input);
     this.#bridge = BridgeMaker.makeBridge(
-      this.#size,
+      this.#bridgeLength,
       BridgeRandomNumberGenerator.generate
     );
+    console.log(this.#bridge);
     this.#location = 0;
     this.getMove();
   }
@@ -34,6 +35,20 @@ class Game {
     const bridgeGame = new BridgeGame(this.#bridge, this.#location);
     const moveSuccess = bridgeGame.move(input);
     OutputView.printMap(this.#bridge, this.#location, moveSuccess);
+    this.#location += 1;
+    this.whatNext(moveSuccess);
+  }
+
+  whatNext(moveSuccess) {
+    if (moveSuccess) {
+      if (this.#location === this.#bridgeLength) {
+        console.log('성공');
+      } else {
+        InputView.readMoving.bind(this)(this.moveOneStep);
+      }
+    } else {
+      console.log('실패');
+    }
   }
 }
 
