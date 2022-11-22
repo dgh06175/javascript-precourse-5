b10e1b5cd0a63e771f3236b066021eacee863b61
내 이름 <  >
2022-11-22 22:16:31 +0900

    refactor: 문자열 상수화

--- src/OutputView.js
+++ src/OutputView.js
@@ -2,17 +2,26 @@ const MissionUtils = require('@woowacourse/mission-utils');
 
 const { Console } = MissionUtils;
 
-/**
- * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
- * 제공된 OutputView 객체를 활용해 구현해야 한다.
- * OutputView의 파일 경로는 변경할 수 있다.
- * OutputView의 메서드의 이름은 변경할 수 없고,  * 인자는 필요에 따라 추가하거나 변경할 수 있다.
- * 값 출력을 위해 필요한 메서드를 추가할 수 있다.
+const START_OUTPUT = '다리 건너기 게임을 시작합니다.';
+const UP_CHAR = 'U';
+const DOWN_CHAR = 'D';
+const O_CHAR = 'O';
+const X_CHAR = 'X';
+const EMPTY_STRING = '';
+const BLANK = ' ';
+const LEFT_WALL = '[';
+const RIGHT_WALL = ']';
+const MIDDLE_WALL = '|';
+const UP_INDEX = 0;
+const DOWN_INDEX = 1;
+const FINAL_GAME_RESULT = '\n최종 게임 결과';
+const GAME_SUCCESS = '\n게임 성공 여부: 성공';
+const GAME_FAILURE = '\n게임 성공 여부: 실패';
+const TOTAL_ATTEMPTS = '총 시도한 횟수: ';
 
- */
 const OutputView = {
   startOutput() {
-    Console.print('다리 건너기 게임을 시작합니다.');
+    Console.print(START_OUTPUT);
   },
   /**
    * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
@@ -20,32 +29,42 @@ const OutputView = {
    * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
   printMap(bridge, location, moveSuccess) {
-    const map = ['', ''];
-    map[0] += this.makeOneLineMap(bridge, location, moveSuccess, 'U');
-    map[1] += this.makeOneLineMap(bridge, location, moveSuccess, 'D');
-    Console.print(map[0]);
-    Console.print(map[1]);
+    const map = [EMPTY_STRING, EMPTY_STRING];
+    map[UP_INDEX] += this.makeOneLineMap(
+      bridge,
+      location,
+      moveSuccess,
+      UP_CHAR
+    );
+    map[DOWN_INDEX] += this.makeOneLineMap(
+      bridge,
+      location,
+      moveSuccess,
+      DOWN_CHAR
+    );
+    Console.print(map[UP_INDEX]);
+    Console.print(map[DOWN_INDEX]);
   },
 
   makeOneLineMap(bridge, location, moveSuccess, upOrDown) {
-    let oneLineMap = '[ ';
-    for (let i = 0; i < location; i += 1) {
-      if (bridge[i] === upOrDown) oneLineMap += 'O';
-      else oneLineMap += ' ';
-      oneLineMap += ' | ';
+    let oneLineMap = LEFT_WALL + BLANK;
+    for (let index = 0; index < location; index += 1) {
+      if (bridge[index] === upOrDown) oneLineMap += O_CHAR;
+      else oneLineMap += BLANK;
+      oneLineMap += BLANK + MIDDLE_WALL + BLANK;
     }
     oneLineMap += this.addLastMap(bridge, location, moveSuccess, upOrDown);
-    oneLineMap += ' ]';
+    oneLineMap += BLANK + RIGHT_WALL;
     return oneLineMap;
   },
 
   addLastMap(bridge, location, moveSuccess, upOrDown) {
     if (moveSuccess) {
-      if (bridge[location] === upOrDown) return 'O';
-      return ' ';
+      if (bridge[location] === upOrDown) return O_CHAR;
+      return BLANK;
     }
-    if (bridge[location] === upOrDown) return ' ';
-    return 'X';
+    if (bridge[location] === upOrDown) return BLANK;
+    return X_CHAR;
   },
 
   /**
@@ -54,12 +73,11 @@ const OutputView = {
    * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
   printResult(bridge, location, isSuccess, attempts) {
-    Console.print('\n최종 게임 결과');
+    Console.print(FINAL_GAME_RESULT);
     this.printMap(bridge, location, isSuccess);
-    if (isSuccess) Console.print('\n게임 성공 여부: 성공');
-    else Console.print('\n게임 성공 여부: 실패');
-    Console.print(`총 시도한 횟수: ${attempts}`);
-    Console.close();
+    if (isSuccess) Console.print(GAME_SUCCESS);
+    else Console.print(GAME_FAILURE);
+    Console.print(`${TOTAL_ATTEMPTS}${attempts}`);
   },
 };
 
