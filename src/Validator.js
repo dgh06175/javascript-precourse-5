ecb2cf5dde7785549f7c52bc5a47c3e6466a9523
내 이름 <  >
2022-11-22 20:02:31 +0900

    feat: 사용자 입력 검사 메소드에 문구 출력 추가

--- src/Validator.js
+++ src/Validator.js
@@ -1,3 +1,6 @@
+const MissionUtils = require('@woowacourse/mission-utils');
+
+const { Console } = MissionUtils;
 const UP_CHAR = 'U';
 const DOWN_CHAR = 'D';
 const RETRY_CHAR = 'R';
@@ -7,19 +10,26 @@ const MAX_BRIDGE_LEN = 20;
 
 const Validator = {
   validateBridgeSizeInput(input) {
+    if (!Number.isInteger(+input)) {
+      Console.print('[ERROR] 3 이상 20 이하의 정수를 입력해주세요.');
+      throw new Error('3 이상 20 이하의 정수를 입력해주세요.');
+    }
     if (+input < MIN_BRIDGE_LEN || +input > MAX_BRIDGE_LEN) {
+      Console.print('[ERROR] 3 이상 20 이하의 정수를 입력해주세요.');
       throw new Error('3 이상 20 이하의 정수를 입력해주세요.');
     }
   },
 
   validateMoving(input) {
     if (input !== DOWN_CHAR && input !== UP_CHAR) {
+      Console.print('[ERROR] U 또는 D를 입력해주세요.');
       throw new Error('U 또는 D를 입력해주세요.');
     }
   },
 
   validateGameCommand(input) {
     if (input !== RETRY_CHAR && input !== QUIT_CHAR) {
+      Console.print('[ERROR] U 또는 D를 입력해주세요.');
       throw new Error('R 또는 Q를 입력해주세요.');
     }
   },
