ad461b599395020fdf0fa9c8793d3848be874f8d
dgh06175 <dgh06175@gmail.com>
2022-12-10 21:54:32 +0900

    refactor: 함수 축약

--- src/App.js
+++ src/App.js
@@ -1,53 +1,45 @@
-const MissionUtils = require("@woowacourse/mission-utils");
+const MissionUtils = require('@woowacourse/mission-utils');
 
 const { Console, Random } = MissionUtils;
 
 class App {
-  playerNumber;
+  #playerNumber;
 
-  computerNumber;
+  #computerNumber;
 
-  isStrike(num, i) {
-    return this.computerNumber[i] === num;
-  }
+  isStrike = (num, i) => this.#computerNumber[i] === num;
 
-  isBall(num, i) {
-    return this.computerNumber[i] !== num && this.computerNumber.includes(num);
-  }
+  isBall = (num, i) =>
+    this.#computerNumber[i] !== num && this.#computerNumber.includes(num);
 
-  isValid(answer) {
-    if (answer.length === 3 && !isNaN(Number(answer))) {
-      return true;
-    }
-    return false;
-  }
+  isValid = (answer) => answer.length === 3 && !isNaN(Number(answer));
 
   constructor() {
-    this.playerNumber = "";
-    this.computerNumber = "";
+    this.#playerNumber = '';
+    this.#computerNumber = '';
     this.result = {};
   }
 
   comNumberInit() {
-    this.computerNumber = "";
-    while (this.computerNumber.length < 3) {
+    this.#computerNumber = '';
+    while (this.#computerNumber.length < 3) {
       const number = Random.pickNumberInRange(1, 9);
-      if (!this.computerNumber.includes(number))
-        this.computerNumber += `${number}`;
+      if (!this.#computerNumber.includes(number))
+        this.#computerNumber += `${number}`;
     }
-    this.computerNumber = [...this.computerNumber];
+    this.#computerNumber = [...this.#computerNumber];
   }
 
   playerInput() {
-    Console.readLine("숫자를 입력해주세요 : ", (answer) => {
+    Console.readLine('숫자를 입력해주세요 : ', (answer) => {
       if (!this.isValid(answer)) {
-        throw new Error("잘못된 입력입니다..");
+        throw new Error('잘못된 입력입니다..');
       }
 
-      this.playerNumber = [...answer];
+      this.#playerNumber = [...answer];
       this.result = {};
 
-      this.playerNumber.forEach((num, i) => {
+      this.#playerNumber.forEach((num, i) => {
         if (this.isStrike(num, i)) {
           this.result.strike = this.result.strike + 1 || 1;
         }
@@ -62,7 +54,7 @@ class App {
   }
 
   printResultText() {
-    let resultText = "";
+    let resultText = '';
     if (this.result.ball > 0) {
       resultText += `${this.result.ball}볼 `;
     }
@@ -72,12 +64,12 @@ class App {
     }
 
     if (!this.result.ball && !this.result.strike) {
-      resultText = "낫싱";
+      resultText = '낫싱';
     }
 
     MissionUtils.Console.print(resultText);
     if (this.result.strike === 3) {
-      MissionUtils.Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+      MissionUtils.Console.print('3개의 숫자를 모두 맞히셨습니다! 게임 종료');
     } else {
       this.playerInput();
     } // 반복
@@ -85,22 +77,21 @@ class App {
 
   continueGame() {
     Console.readLine(
-      "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n",
+      '게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n',
       (answer) => {
-        if (answer === "1") {
+        if (answer === '1') {
           this.comNumberInit();
           this.playerInput(); // 재귀
         }
-        if (answer === "2") {
-          MissionUtils.Console.print("게임 종료");
+        if (answer === '2') {
+          MissionUtils.Console.print('게임 종료');
         }
-        return;
       }
     );
   }
 
   play() {
-    MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
+    MissionUtils.Console.print('숫자 야구 게임을 시작합니다.');
     this.comNumberInit();
     this.playerInput();
     MissionUtils.Console.close();
